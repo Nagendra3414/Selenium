c9cee73962553b8c777a22d76c93b96a2515aa19
Nagendra <gadinagendra333@gmail.com>
2022-12-27 21:09:23 +0530

    First commit

---
+++ src/appium/Basic.java
@@ -0,0 +1,13 @@
+package appium;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+	}
+
+}
